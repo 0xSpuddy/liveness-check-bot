ce78db9eb0e0269179daa4d6e6583da53103df25
0xSpuddy <sloetterle@tellor.io>
2024-08-21 11:06:07 -0400

    async working

--- check_everything.py
+++ check_everything.py
@@ -1,11 +1,21 @@
 import time
 import requests
+from dotenv import load_dotenv
+import os
+import tracemalloc
+import asyncio
 from discordwebhook import Discord
 from datetime import datetime
 
-DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1275539999235768390/<yourwebhook>"
+load_dotenv()
 
-def check_status(url, timeout=5):
+tracemalloc.start()
+
+DISCORD_HOOK = os.getenv("DISCORD_WEBHOOK_URL")
+URL = os.getenv("URL")
+
+
+async def check_status(url, timeout=5):
     try:
         response = requests.get(url, timeout=timeout)
         # Consider the URL functional if the response code is in the 200-299 range
@@ -18,32 +28,25 @@ def check_status(url, timeout=5):
     except requests.ConnectionError:
         return "error", None
 
-def main():
-    urls_to_check = [
-        "https://url1...",
-        "https://url2...",
-        "https://url3...",
-    ]
-
-    alert_bot = Discord(url=DISCORD_WEBHOOK_URL)
+async def main() -> None:
+    alert_bot = Discord(url=DISCORD_HOOK)
     while True:
-        for url in urls_to_check:
-            status, code = check_status(url)
-            current_time = datetime.now()
-            formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
-            if status == "up":
-                print(f"{url} is up and functional (Status code: {code})")
-            elif status == "down":
-                print(f"{url} is down (Status code: {code})")
-                alert_bot.post(content=f"{url} is down at {formatted_time} (Status code: {code})")
-            elif status == "timeout":
-                print(f"{url} is down (Request timed out)")
-                alert_bot.post(content=f"{url} is down at {formatted_time} (Request timed out)")
-            elif status == "error":
-                print(f"{url} is down (Connection error)")
-                alert_bot.post(content=f"{url} is down at {formatted_time} (Connection error)")
+        url = URL
+        status, code = await check_status(url)
+        current_time = datetime.now()
+        formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+        if status == "up":
+            print(f"{url} is up and functional (Status code: {code})")
+        elif status == "down":
+            print(f"{url} is down (Status code: {code})")
+            alert_bot.post(content=f"{url} is down at {formatted_time} (Status code: {code})")
+        elif status == "timeout":
+            print(f"{url} is down (Request timed out)")
+            alert_bot.post(content=f"{url} is down at {formatted_time} (Request timed out)")
+        elif status == "error":
+            print(f"{url} is down (Connection error)")
+            alert_bot.post(content=f"{url} is down at {formatted_time} (Connection error)")
         
-        time.sleep(300)  # Wait for 10 seconds before checking again
+        await asyncio.sleep(10)
 
-if __name__ == "__main__":
-    main()
+asyncio.run(main())
